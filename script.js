e40758827d1bb987d8b58b013cfac3d5a2ebd5bb
kmvanneste <kmvanneste17@gmail.com>
2020-07-26 17:29:38 -0400

    added initial functions

---
+++ script.js
@@ -0,0 +1,43 @@
+var startContainer = document.getElementById('start-container');
+var startButton = document.getElementById('start-btn');
+
+var topRow = document.getElementById('top-row');
+
+var questionContainer = document.getElementById('question-container');
+
+var endContainer = document.getElementById('end-container');
+
+startButton.addEventListener("click", startQuiz);
+
+
+function startQuiz() {
+    startContainer.classList.add('hide');
+    topRow.classList.remove('hide');
+    questionContainer.classList.remove('hide');
+
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
+
+
+
+
+function submitScore() {
+
+}
+
+function viewHighscores() {
+
+}
+
+function goBack() {
+
+}
+
